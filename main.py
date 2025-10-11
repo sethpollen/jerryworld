ad6d77b5d675350694d10a4b880bf9802e9b15c7
jeremiahpollen <jeremiah.pollen@gmail.com>
2025-10-11 14:17:54 -0500

    cool coder
    ball that moves

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
-#!/usr/bin/env python3
-
+# Example file showing a circle moving on screen
 import pygame
-import sys
 
+# pygame setup
 pygame.init()
-pygame.display.set_caption("jerryworld")
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-screen = pygame.display.set_mode(SCREEN_SIZE)
-
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-square_rect = pygame.Rect(10, 10, 50, 50)
-
-# Game loop.
-while True:
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            break
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                break
+            running = False
 
-    # Draw a red square on a black background.
-    screen.fill(BLACK)
-    pygame.draw.rect(screen, RED, square_rect)
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
 
-    # Update the display.
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
     pygame.display.flip()
 
-# Clean up and exit.
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
 pygame.quit()
-sys.exit()
